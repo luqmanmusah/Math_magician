75188fca5c362b105488f85d252644c2e5758b45
Luqman Musah <musahluqman@gmail.com>
2021-08-23 12:00:31 +0000

    Add background colors

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -14,26 +14,26 @@ class Calculator extends React.Component {
         <button name="AC" type="button">AC</button>
         <button name="+/-" type="button">+/-</button>
         <button name="%" type="button">%</button>
-        <button name="/" type="button">/</button>
+        <button name="/" type="button" className="operators">/</button>
 
         <button name="7" type="button">7</button>
         <button name="8" type="button">8</button>
         <button name="9" type="button">9</button>
-        <button name="*" type="button">*</button>
+        <button name="*" type="button" className="operators">*</button>
 
         <button name="4" type="button">4</button>
         <button name="5" type="button">5</button>
         <button name="6" type="button">6</button>
-        <button name="-" type="button">-</button>
+        <button name="-" type="button" className="operators">-</button>
 
         <button name="1" type="button">1</button>
         <button name="2" type="button">2</button>
         <button name="3" type="button">3</button>
-        <button name="+" type="button">+</button>
+        <button name="+" type="button" className="operators">+</button>
 
         <button name="0" type="button" id="zero">0</button>
         <button name="." type="button">.</button>
-        <button name="=" type="button">=</button>
+        <button name="=" type="button" className="operators">=</button>
       </div>
     );
 }
